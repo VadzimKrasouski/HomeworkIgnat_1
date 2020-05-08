53865afa0eb4d4358305409501ed7e268a75f819
VadzimKrasouski <vadzim.krasouski@gmail.com>
2020-05-08 10:04:25 +0200

    Updates

---
+++ precache-manifest.df683e131162c9d218f389d83ea3a9ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f2eb108a953ff2ce4b0c94a569ffe1",
+    "url": "/HomeworkIgnat_1/index.html"
+  },
+  {
+    "revision": "02ece4882fb19669b113",
+    "url": "/HomeworkIgnat_1/static/css/main.7b0df35d.chunk.css"
+  },
+  {
+    "revision": "177f35551654d0d58617",
+    "url": "/HomeworkIgnat_1/static/js/2.a553c841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HomeworkIgnat_1/static/js/2.a553c841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ece4882fb19669b113",
+    "url": "/HomeworkIgnat_1/static/js/main.7ae71203.chunk.js"
+  },
+  {
+    "revision": "f07e8fb480dffa1d9fdb",
+    "url": "/HomeworkIgnat_1/static/js/runtime-main.d7ebf781.js"
+  },
+  {
+    "revision": "ed2c3db610cd08f1d9c02b60a8658a63",
+    "url": "/HomeworkIgnat_1/static/media/react.ed2c3db6.jpeg"
+  }
+]);
